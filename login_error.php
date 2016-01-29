caf8d187346b5932df5725d91e4285c82dcc53db
jhe15 <hejin688@yahoo.com>
2016-01-29 14:37:33 -0800

    Create login_error.php

---
+++ login_error.php
@@ -0,0 +1,29 @@
+<!--author Jin He -->
+
+<div class="tit">Login</div>    
+<hr>
+
+
+<div class="title">
+user name:<br>
+
+
+<form action="./modules/login/ValidateUser.php" method="POST">
+
+<input id="upload" type="text" name="state"> <br>
+
+
+password:<br>
+
+
+<input id="upload" type="password" name="manu"> <br>
+<input id="upload" type="submit" value="login">
+<input id="upload" type="reset" value="clear">
+</form>
+</div>
+
+<img src="./images/badpassword.png">
+
+
+
+
