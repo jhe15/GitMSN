6b7d609db2d537f63c3817c589dd0c08b2f3fc48
jhe15 <hejin688@yahoo.com>
2016-01-29 14:38:07 -0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,22 @@
+<!--author Jin He -->
+
+<div class="tit">Register</div>    
+<hr>
+
+<div class="title">
+user name:<br>
+
+
+<form action="./modules/login/RegisterUser.php" method="POST">
+
+<input id="upload" type="text" name="state"> <br>
+
+
+password:<br>
+
+
+<input id="upload" type="password" name="manu"> <br>
+<input id="upload" type="submit" value="submit">
+<input id="upload" type="reset" value="clear">
+</form>
+</div>
